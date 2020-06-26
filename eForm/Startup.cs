06338a94acb0fe228b1ea5cf0e850ede18e20aba
max <max@BNKDINISCMAX>
2020-06-26 14:18:27 +0700

    Add project files.

---
+++ eForm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eForm.Startup))]
+namespace eForm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
